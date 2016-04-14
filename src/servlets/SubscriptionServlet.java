4ae0f5cc4667da16451e329830b6c2e06a20ae73
jack <khajagbasmajian@yahoo.com>
2016-04-13 22:34:34 -0700

    update

---
+++ src/servlets/SubscriptionServlet.java
@@ -0,0 +1,195 @@
+/* 
+ * Class: 			LighthouseServlet
+ * Author:			Khajag Basmajian and Sevan Gregorian 
+ * Date Created:	04-13-2016
+ * Purpose:			Handle GET and POST requests from servlet subscriptions.jsp
+ * 					Query The database for subscribed movies
+ * 
+ * */
+
+package servlets;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SimpleTimeZone;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@SuppressWarnings("serial")
+public class SubscriptionServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException, ServletException {
+
+
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSSSSS");
+		fmt.setTimeZone(new SimpleTimeZone(0, ""));
+
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String loginUrl = userService.createLoginURL("/");
+		String logoutUrl = userService.createLogoutURL("/");
+
+
+		ArrayList<Movie> subscribedMovies = new ArrayList<Movie>();
+		
+		req.setAttribute("user", user);
+		req.setAttribute("loginUrl", loginUrl);
+		req.setAttribute("logoutUrl", logoutUrl);
+
+		if (user != null) {
+			try {
+				DatabaseHandler.addUser(user.getEmail());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}				
+		}
+		try {
+			DatabaseHandler.printUsers();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		if (user != null) {
+			try {
+				subscribedMovies = DatabaseHandler.getListofSubscriptions(user.getEmail());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+
+		try {
+			subscribedMovies = this.getMovieImg(subscribedMovies);
+		} catch (ParseException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		req.setAttribute("subscribedMovies", subscribedMovies);
+		req.setAttribute("currentTime", fmt.format(new Date()));
+
+		resp.setContentType("text/html");
+
+		RequestDispatcher jsp = req.getRequestDispatcher("/WEB-INF/subscriptions.jsp");
+		jsp.forward(req, resp);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		/*
+		 * Method Name: doPost() Author: Harout Grigoryan Date Created:
+		 * 03-16-2016 Purpose: Take homepage's POST requests which include
+		 * subscribing/unsubscribing then use DatabaseHandler to update the DB
+		 * accordingly. Input: HTTP request and response which include
+		 * attributes (such as the movie's information for subscribing) Return:
+		 * method is void, redirects to the doGet
+		 */
+
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		String userEmail = user.getEmail();
+
+		// get movie's title, image URL, subscription status, release date
+		// from client based on the movie they are unsubscribing/subscribing to
+		String movieTitle = req.getParameter("title");
+		String movieImg = req.getParameter("imgUrl");
+		String susbcribed = req.getParameter("subscribed");
+		Date movieDate = null;
+		try {
+			movieDate = this.parseDate(req.getParameter("releaseDate"),
+					"EEE MMM dd kk:mm:ss zzz yyyy");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Movie movie = new Movie(movieTitle, movieDate, movieImg);
+
+		try {
+			if (susbcribed.equalsIgnoreCase("false")) {
+				DatabaseHandler.addSubscription(movie, userEmail);
+			} else {
+				DatabaseHandler.deleteSubscription(movie, userEmail);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		resp.sendRedirect("/subscriptions");
+	}
+
+	private ArrayList<Movie> getMovieImg(ArrayList<Movie> movieList)
+			throws MalformedURLException, IOException, ParseException,
+			SQLException {
+
+
+		for (int i = 0; i < movieList.size(); i++) {
+			String url = "http://api.themoviedb.org/3/search/movie?api_key=59471fd0915a80b420b392a5db81f1c2&query="
+					+ movieList.get(i).title;
+			String json = IOUtils.toString(new URL(url));
+			JsonParser parser = new JsonParser();
+
+			// Element is the root node of the parsed JSON
+			// Using element we can access the keys/values in the JSON
+			JsonElement element = parser.parse(json);
+
+			if (element.isJsonObject()) {
+				JsonObject pages = element.getAsJsonObject();
+				JsonArray movies = pages.getAsJsonArray("results");
+				JsonObject dataset = movies.get(0).getAsJsonObject();
+
+				Movie x = movieList.get(i);
+
+				if (!dataset.get("poster_path").isJsonNull()) {
+					String imgUrl = dataset.get("poster_path").getAsString();
+					x.setImgUrl("http://image.tmdb.org/t/p/w500/" + imgUrl);
+				} 
+				else
+				x.setImgUrl("https://placehold.it/200x300?text=Movie"); 
+			}
+		}
+
+		return movieList;
+	}
+
+	private Map<String, SimpleDateFormat> hashFormatters = new HashMap<String, SimpleDateFormat>();
+
+	public Date parseDate(String date, String format) throws ParseException {
+		/*
+		 * Method Name: parseDate() Author: Harout Grigoryan Date Created:
+		 * 03-16-2016 Purpose: Creates Date formatted object based on given date
+		 * string Input: String with date and desired format for Date Return:
+		 * Formatted Date object
+		 */
+
+		SimpleDateFormat formatter = hashFormatters.get(format);
+
+		if (formatter == null) {
+			formatter = new SimpleDateFormat(format);
+			hashFormatters.put(format, formatter);
+		}
+
+		return formatter.parse(date);
+	}
+}
---
+++ war/WEB-INF/subscriptions.jsp
@@ -0,0 +1,160 @@
+<!-- 
+ Name: 		    	subscription.jsp
+ Author:			Harout Grigoryan
+ Date Created:  	03-14-2016
+ Purpose:			Homepage for Lighthouse, serves for subscribing/unsubscribing,
+ 					Logging in/out, and searching movies.
+ -->
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Lighthouse</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/3-col-portfolio.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">Lighthouse</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                        <c:choose>
+							<c:when test="${user != null}">
+							<li>
+								<a href="#" style="color:#FFF;">Welcome, ${user.email}!</a>
+							</li>
+							<li>
+								<a href="${logoutUrl}" style="color:#FFF;">Sign Out</a>.
+							</li>
+							</c:when>
+							<c:otherwise>
+								<li>
+									<a href="${loginUrl }"><img src="img/btn_google_signin_light_normal_web.png" alt="Google Signin"></a>
+								</li>
+							</c:otherwise>
+						</c:choose>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Header -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">My Subscriptions 
+                	<h4>Here is a list of movies you are currently subscribed to!</h4>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+	<c:forEach items="${subscribedMovies}" var="movie" varStatus="loop">
+	        <!-- Projects Row -->
+	        <div class="row">
+	            <div class="col-md-4 portfolio-item">
+	                <img src="${movie.imgUrl}" style="height:200px;">
+	            	<!--  if release date is after current date -->
+					<jsp:useBean id="now" class="java.util.Date"/>
+					<c:choose>
+						<c:when test="${movie.releaseDate gt now}">
+							<h3>
+							<form action="/" method="post">
+									<input type="hidden" name="title" value="${movie.title}" />
+									<input type="hidden" name="releaseDate" value="${movie.releaseDate}" />
+									<input type="hidden" name="imgUrl" value="${movie.imgUrl}" />
+									<input type="hidden" name="subscribed" value="true" />
+									<button type="submit" value="Submit" class="btn btn-danger btn-lg" style="width:150px">Unsubscribe</button>
+							</form>
+							</h3>
+						</c:when>
+						<c:otherwise>
+							<h3>
+							Already Released!
+							</h3>
+						</c:otherwise>
+					</c:choose>
+	            </div>    
+	   
+				<div class="col-md-10 col-sm-9">
+				  <h3>${movie.title}</h3>
+				  <div class="row">
+					<div class="col-xs-9">
+					  <h4>
+					  <fmt:formatDate type="date" value="${movie.releaseDate}"/>
+					  </h4>	
+					</div>
+				  </div>
+				  <br><br>
+				</div>
+	        </div>
+	        <!-- /.end row -->
+	        
+	</c:forEach>
+        
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Lighthouse 2016</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
